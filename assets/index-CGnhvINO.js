9ecb1855d69b4d2985bb4bdc5018b140a28ef7cd
loonghe <loooonghe@gmail.com>
2026-01-20 13:55:20 +0800

    website

---
+++ assets/index-CGnhvINO.js
@@ -0,0 +1,29 @@
+import{a as n,d as m,K as d,x as u,f as t,s,y as h,N as l,a6 as y,g as r,v}from"./index-66zwFF_T.js";import{E as f}from"./card-6qHA2QcG.js";import"./purify.es-BB1RUjTX.js";import"./ahooks-vue.es-C7FZNFHb.js";import{C as x}from"./custom-rich-text-BFBya3g_.js";import"./index-3B-DsrMj.js";import"./scroll-OujtyDAQ.js";import"./debounce-DKSyZsSg.js";import"./toNumber-BxxjEsNI.js";const g=""+new URL("vertical-image-BKOXr2Do.png",import.meta.url).href,w=""+new URL("horizon-image-CZ-oJcMu.png",import.meta.url).href,_=""+new URL("horizon-video-4BSrP97H.mov",import.meta.url).href,R=""+new URL("vertical-video-ZTABouOh.mov",import.meta.url).href,i=o=>new URL(o,import.meta.url).href,U=n(_),E=`<h2>欢迎使用富文本渲染组件！</h2>
+<ul data-list-style-type="circle" class="nonUnicode-list-style-type">
+  <li><p>防xss攻击</p></li>
+</ul>
+<ul data-list-style-type="circle" style="--ul-list-style-type: '\\25EF';" class="nonUnicode-list-style-type">
+  <li><p>图片预览和视频播放</p></li>
+</ul>
+<ul data-list-style-type="circle" style="--ul-list-style-type: '\\25EF';" class="nonUnicode-list-style-type">
+  <li><p>图片/视频真实长宽比渲染</p></li>
+</ul>
+<p><span style="color: #999999">这是一张竖屏图片和一张横屏图片</span></p>
+<p>
+  <span class="exeditor-picture">
+    <img src="${i(g)}" width="303" height="729">
+  </span>
+  <span class="exeditor-picture">
+    <img src="${i(w)}" width="482" height="158">
+  </span>
+</p>
+<p><span style="color: #999999">这是一个竖屏视频和一个横屏视频</span></p>
+<p>
+  <span class="exeditor-video">
+    <video src="${i(R)}" width="788" height="1576" controls="true" data-inline=""></video>
+  </span>
+  <span class="exeditor-video">
+    <video src="${U.value}" width="2572" height="1576" controls="true" data-inline=""></video>
+  </span>
+</p>
+<p></p>`,F=m({__name:"index",setup(o){const a=n(E);return(B,e)=>{const p=f,c=h;return v(),d(y,null,[e[2]||(e[2]=u("h3",null,"富文本渲染",-1)),t(p,null,{default:s(()=>[...e[0]||(e[0]=[r(" 简要介绍 + 多种编辑器模式选择：简洁模式、默认模式、模拟腾讯文档模式、划词笔记模式（新窗口打开） + 支持图片、视频上传功能 + 支持划词批注功能（新窗口打开） + 支持MarkDown语法 + 支持@提及功能 ",-1)])]),_:1}),t(c,null,{default:s(()=>[...e[1]||(e[1]=[r("Example",-1)])]),_:1}),t(l(x),{html:l(a)},null,8,["html"])],64)}}});export{F as default};
