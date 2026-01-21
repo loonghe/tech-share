2d37f467a3e323bc423c15b4a25d93ac157cc036
loonghe <loooonghe@gmail.com>
2026-01-22 00:18:40 +0800

    test build

---
+++ assets/js/index-DNuiLVOT.js
@@ -0,0 +1,29 @@
+import"./index-OIiAUsjm.js";/* empty css             */import"./custom-list.vue_vue_type_style_index_0_scoped_5ede2967_lang-C96gZMQD.js";import{s as c,e as m}from"./ui-hcfEnn37.js";import{C as d}from"./custom-rich-text-IAZ1VwAq.js";import{C as u}from"./custom-list-DTGVF9DA.js";import{a as h}from"./const-m63Ke8bE.js";import{r as a,x as y,C as f,E as v,Y as t,O as l,u as s,G as x,a4 as g,A as _}from"./vue-DOVs0rbj.js";const w=""+new URL("../images/vertical-image-BKOXr2Do.png",import.meta.url).href,R=""+new URL("../images/horizon-image-CZ-oJcMu.png",import.meta.url).href,C=""+new URL("../mov/horizon-video-Dj-54aj2.mov",import.meta.url).href,U=""+new URL("../mov/vertical-video-B9xMeWif.mov",import.meta.url).href,i=o=>new URL(o,import.meta.url).href,L=a(C),E=`<h2>欢迎使用富文本渲染组件！</h2>
+<ul data-list-style-type="circle" class="nonUnicode-list-style-type">
+  <li><p>防xss攻击</p></li>
+</ul>
+<ul data-list-style-type="circle" style="--ul-list-style-type: '\\25EF';" class="nonUnicode-list-style-type">
+  <li><p>图片预览和视频播放</p></li>
+</ul>
+<ul data-list-style-type="circle" style="--ul-list-style-type: '\\25EF';" class="nonUnicode-list-style-type">
+  <li><p>图片/视频真实长宽比渲染</p></li>
+</ul>
+<p><span style="color: #999999">这是一张竖屏图片和一张横屏图片</span></p>
+<p>
+  <span class="exeditor-picture">
+    <img src="${i(w)}" width="303" height="729">
+  </span>
+  <span class="exeditor-picture">
+    <img src="${i(R)}" width="482" height="158">
+  </span>
+</p>
+<p><span style="color: #999999">这是一个竖屏视频和一个横屏视频</span></p>
+<p>
+  <span class="exeditor-video">
+    <video src="${i(U)}" width="788" height="1576" controls="true" data-inline=""></video>
+  </span>
+  <span class="exeditor-video">
+    <video src="${L.value}" width="2572" height="1576" controls="true" data-inline=""></video>
+  </span>
+</p>
+<p></p>`,k=y({__name:"index",setup(o){const r=a(E);return(B,e)=>{const n=c,p=m;return _(),f(x,null,[e[1]||(e[1]=v("h3",null,"富文本渲染",-1)),t(n,null,{default:l(()=>[t(s(u),{items:s(h)},null,8,["items"])]),_:1}),t(p,null,{default:l(()=>[...e[0]||(e[0]=[g("Example",-1)])]),_:1}),t(s(d),{html:s(r)},null,8,["html"])],64)}}});export{k as default};
