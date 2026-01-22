2bfd5fdb628b3ca3ad438da067b44d3b69a65ea4
loonghe <loooonghe@gmail.com>
2026-01-22 15:30:20 +0800

    use cdn

---
+++ assets/js/index-CZVIGrCO.js
@@ -0,0 +1,29 @@
+import"./index-N1i5mGQw.js";/* empty css             */import"./custom-list.vue_vue_type_style_index_0_scoped_5ede2967_lang-7-oiKZVs.js";import{s as c,e as d}from"./ui-5xhcnyUA.js";import{C as h}from"./custom-rich-text-BvFCTybI.js";import{C as m}from"./custom-list-CdAWI7Q9.js";import{a as u}from"./const-m63Ke8bE.js";import{r as a,x as g,C as v,E as y,Y as t,O as l,u as s,G as x,a4 as f,A as _}from"./vue-DUJZPJ4W.js";const C="https://cdn.jsdelivr.net/gh/loonghe/tech-share@gh-pages/assets/images/vertical-image-BKOXr2Do.png",E="https://cdn.jsdelivr.net/gh/loonghe/tech-share@gh-pages/assets/images/horizon-image-CZ-oJcMu.png",j="https://cdn.jsdelivr.net/gh/loonghe/tech-share@gh-pages/assets/mov/horizon-video-Dj-54aj2.mov",w="https://cdn.jsdelivr.net/gh/loonghe/tech-share@gh-pages/assets/mov/vertical-video-B9xMeWif.mov",i=o=>new URL(o,import.meta.url).href,R=a(j),B=`<h2>欢迎使用富文本渲染组件！</h2>
+<ul data-list-style-type="circle" class="nonUnicode-list-style-type">
+  <li><p>防xss攻击</p></li>
+</ul>
+<ul data-list-style-type="circle" style="--ul-list-style-type: '\\25EF';" class="nonUnicode-list-style-type">
+  <li><p>图片预览和视频播放</p></li>
+</ul>
+<ul data-list-style-type="circle" style="--ul-list-style-type: '\\25EF';" class="nonUnicode-list-style-type">
+  <li><p>图片/视频真实长宽比渲染</p></li>
+</ul>
+<p><span style="color: #999999">这是一张竖屏图片和一张横屏图片</span></p>
+<p>
+  <span class="exeditor-picture">
+    <img src="${i(C)}" width="303" height="729">
+  </span>
+  <span class="exeditor-picture">
+    <img src="${i(E)}" width="482" height="158">
+  </span>
+</p>
+<p><span style="color: #999999">这是一个竖屏视频和一个横屏视频</span></p>
+<p>
+  <span class="exeditor-video">
+    <video src="${i(w)}" width="788" height="1576" controls="true" data-inline=""></video>
+  </span>
+  <span class="exeditor-video">
+    <video src="${R.value}" width="2572" height="1576" controls="true" data-inline=""></video>
+  </span>
+</p>
+<p></p>`,k=g({__name:"index",setup(o){const n=a(B);return(T,e)=>{const r=c,p=d;return _(),v(x,null,[e[1]||(e[1]=y("h3",null,"富文本渲染",-1)),t(r,null,{default:l(()=>[t(s(m),{items:s(u)},null,8,["items"])]),_:1}),t(p,null,{default:l(()=>[...e[0]||(e[0]=[f("Example",-1)])]),_:1}),t(s(h),{html:s(n)},null,8,["html"])],64)}}});export{k as default};
